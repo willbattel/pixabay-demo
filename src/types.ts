1d44a8398af648b38a3c2fded10f78ac44784d6c
Will Battel <willbattel@gmail.com>
2023-08-12 18:35:52 -0400

    Create types for Pixabay query results

---
+++ src/types.ts
@@ -0,0 +1,30 @@
+export interface PixabaySearchResults {
+    total: number
+    totalHits: number
+    hits: PixabaySearchResultItem[]
+}
+
+export interface PixabaySearchResultItem {
+    id: number
+    pageURL: string
+    type: string
+    tags: string
+    previewURL: string
+    previewWidth: number
+    previewHeight: number
+    webformatURL: string
+    webformatWidth: number
+    webformatHeight: number
+    largeImageURL: string
+    imageWidth: number
+    imageHeight: number
+    imageSize: number
+    views: number
+    downloads: number
+    collections: number
+    likes: number
+    comments: number
+    user_id: number
+    user: string
+    userImageURL: string
+}
