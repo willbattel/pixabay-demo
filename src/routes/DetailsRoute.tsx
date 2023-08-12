2d703c82dfc9c3de90e916a7edf24ad612863b43
Will Battel <willbattel@gmail.com>
2023-08-12 19:20:06 -0400

    Populate DetailsRoute

--- src/routes/DetailsRoute.tsx
+++ src/routes/DetailsRoute.tsx
@@ -1,7 +1,50 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { PixabaySearchResultItem } from "../types";
+
+interface State {
+    pixabayItem: PixabaySearchResultItem
+}
+
 export default function DetailsRoute() {
+    const { state, pathname } = useLocation()
+    const [ pixabayItem, setPixabayItem ] = useState<PixabaySearchResultItem | null>(null)
+
+    const itemId = pathname.split("/")[1]
+
+    useEffect(() => {
+        if (state) {
+            const { pixabayItem } = state as State
+            if (pixabayItem.id.toString() === itemId) {
+                setPixabayItem(pixabayItem)
+            }
+            else {
+                console.error("Pixabay item ID does not match that of pathname.")
+            }
+        }
+        else {
+            getPixabayItem()
+        }
+    }, [itemId, setPixabayItem, state])
+
+    function getPixabayItem() {
+        // TODO
+    }
+
+    if (!pixabayItem) {
+        return (
+            <h2>Loading image details</h2>
+        )
+    }
+
     return (
-        <p>
-            TODO: Details
-        </p>
+        <div>
+            <h2>Image details</h2>
+            <ul>
+                <li>User: {pixabayItem.user}</li>
+                <li>Tags: {pixabayItem.tags}</li>
+            </ul>
+            <img src={pixabayItem.largeImageURL} />
+        </div>
     )
 }
