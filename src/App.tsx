ff32172abcead8c1dd54978d838550d0e2228c10
Will Battel <willbattel@gmail.com>
2023-08-12 18:06:04 -0400

    Add Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom'
 
 import './App.css'
+import Header from './components/Header'
 
 export default function App() {
     return (
         <div>
+            <Header/>
             <Outlet />
         </div>
     )
