5d26a06831de232d9921690738c45f9396e5aeda
Will Battel <willbattel@gmail.com>
2023-08-12 18:31:14 -0400

    Add search query form

--- src/routes/SearchRoute.tsx
+++ src/routes/SearchRoute.tsx
@@ -1,7 +1,24 @@
 export default function SearchRoute() {
+
+    function performSearch() {
+        const formElement = document.getElementById("searchForm") as HTMLFormElement
+        const formData = new FormData(formElement)
+        const searchQuery = formData.get('searchQueryInput') as string
+        
+        // TODO: Perform search
+    }
+
     return (
         <div>
-            TODO: Search
+            <div>
+                <form id="searchForm">
+                    <input name="searchQueryInput" type="search" placeholder="Search for images" />
+                </form>
+                <button onClick={performSearch}>Search</button>
+            </div>
+            <div>
+                TODO: Search results
+            </div>
         </div>
     )
 }
