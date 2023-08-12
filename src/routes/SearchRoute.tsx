cd245e74462ade4f4a962f49fdb62bcef6bda8e0
Will Battel <willbattel@gmail.com>
2023-08-12 18:48:11 -0400

    Build Pixabay request

--- src/routes/SearchRoute.tsx
+++ src/routes/SearchRoute.tsx
@@ -1,11 +1,28 @@
+import { pixabayApiKey } from "../secrets"
+
 export default function SearchRoute() {
 
-    function performSearch() {
+    async function performSearch() {
+        // Get raw user input from form
         const formElement = document.getElementById("searchForm") as HTMLFormElement
         const formData = new FormData(formElement)
         const searchQuery = formData.get('searchQueryInput') as string
+
+        // Get sanitized, non-empty search components from input string
+        const searchComponents = searchQuery
+            .replace(/[^a-z0-9áéíóúñü ]/gim, "")
+            .split(" ")
+            .filter(component => !!component)
         
-        // TODO: Perform search
+        // Ignore empty queries
+        if (searchComponents.length < 1) {
+            return
+        }
+
+        // Build URL and send request
+        const url = `https://pixabay.com/api/?key=${pixabayApiKey}&q=${searchComponents.join("+")}&image_type=photo`
+
+        // TODO: Fetch
     }
 
     return (
