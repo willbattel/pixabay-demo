6e888f29bbe61adfc7d9086fa903556edfe56404
Will Battel <willbattel@gmail.com>
2023-08-12 20:16:49 -0400

    Show error message

--- src/routes/DetailsRoute.tsx
+++ src/routes/DetailsRoute.tsx
@@ -17,6 +17,7 @@ export default function DetailsRoute() {
     const navigate = useNavigate()
     const { state, pathname } = useLocation()
     const [ pixabayItem, setPixabayItem ] = useState<PixabaySearchResultItem | null>(null)
+    const [ didFinishLoading, setDidFinishLoading ] = useState<boolean>(false)
 
     // See above note regarding application state management.
     const pixabayItems = useRef<PixabaySearchResultItem[] | null>(null)
@@ -26,26 +27,34 @@ export default function DetailsRoute() {
 
     const getPixabayItem = useCallback(async () => {
         if (!itemId) {
+            setDidFinishLoading(true)
             console.error("Unable to determine the item ID.")
             return
         }
         
         // Build URL and send request
         const url = `https://pixabay.com/api/?key=${pixabayApiKey}&id=${itemId}&image_type=photo`
-        const response = await fetch(url, {
-            method: "GET",
-        })
-        const results = await response.json() as PixabayResponse
-        const item = results.hits[0]
-
-        if (!item) {
-            // TODO: Render error
-            console.error("No images found with ID.")
-            return
+        try {
+            const response = await fetch(url, {
+                method: "GET",
+            })
+            const results = await response.json() as PixabayResponse
+            const item = results.hits[0]
+    
+            if (!item) {
+                setDidFinishLoading(true)
+                console.error("No images found with ID.")
+                return
+            }
+    
+            // Re-render with item details
+            setPixabayItem(item)
+            setDidFinishLoading(true)
         }
-
-        // Re-render with item details
-        setPixabayItem(item)
+        catch (err) {
+            setDidFinishLoading(true)
+            console.error(err)
+        }
     }, [itemId])
 
     useEffect(() => {
@@ -58,6 +67,7 @@ export default function DetailsRoute() {
             else {
                 console.error("Pixabay item ID does not match that of pathname.")
             }
+            setDidFinishLoading(true)
             pixabayItems.current = _state.pixabayItems
             searchQuery.current = _state.searchQuery
         }
@@ -76,13 +86,22 @@ export default function DetailsRoute() {
         })
     }
 
-    if (!pixabayItem) {
+    if (!didFinishLoading) {
         return (
             <div>
                 <h2>Loading image details</h2>
                 <button onClick={backToSearch}>Back to search</button>
             </div>
-            
+        )
+    }
+
+    if (!pixabayItem) {
+        return (
+            <div>
+                <h2>There was a problem loading the image.</h2>
+                <p>Check the console for details.</p>
+                <button onClick={backToSearch}>Back to search</button>
+            </div>
         )
     }
 
