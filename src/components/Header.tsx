2abfeee87c907c27efc78287bc7ebdb142d98d03
Will Battel <willbattel@gmail.com>
2023-08-12 20:20:14 -0400

    Add index anchor to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom"
+
 export default function Header() {
     return (
-        <div>
-            <h1>Pixabay Demo</h1>
-        </div>
+        <Link to="/">
+            <div>
+                <h1>Pixabay Demo</h1>
+            </div>
+        </Link>
     )
 }
